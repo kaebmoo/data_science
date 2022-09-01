20fd50fbca2810868a0a87d0f35e01097438bb37
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2022-09-01 07:50:26 +0700

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -5,13 +5,13 @@ import pandas as pd # นำเข้า library pandas
 data = pd.read_csv('C:\DW\TRN_EXPENSE_BEFORE_20220731.csv') # อ่านไฟล์ ไปไว้ตัวแปร data
 lookup = pd.read_csv('C:\DW\EXPENSE_GL_CODE_NT1_NT.csv', encoding= 'tis-620') # อ่านไฟล์ ไปไว้ใน lookup
 
-# print(df.to_string()) 
-# print(df)
+# print(data.to_string()) 
+# print(data)
 
 print(data.head()) # พิมพ์ โครงสร้างไฟล์ data ออกมาดู
 print(lookup.head())
 
-print("Grand Total = ", end=" ")
+## print("Grand Total = ", end=" ")
 print(data['EXPENSE_VALUE'].sum())
 
 print("Grand Total = ", "{:,.2f}".format(data['EXPENSE_VALUE'].sum()))
