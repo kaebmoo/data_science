f640f099f1c2cdebf43dc7860df36357bfe9126c
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2022-09-06 22:24:18 +0700

    Update read_html.py

--- read_html.py
+++ read_html.py
@@ -2,7 +2,6 @@
 
 import pandas as pd
 import numpy as np
-
 from unicodedata import normalize
 
 table = pd.read_html("inquiry.html")
@@ -16,6 +15,9 @@ print(df.info())
 
 print(df[df.PERIOD == "2022/01"])
 
-print(df.query('PERIOD == "2022/01"'))
+print(df.query('PERIOD == "2022/01" and PROCESS == "completed"'))
 row = df.query('PERIOD == "2022/01"')
 print(row.PAID_AMOUNT)
+
+print()
+print(row.PAID_AMOUNT+1)
