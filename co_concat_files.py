aec46fab33ff14bb48bdceeb2ba805f1a314e93f
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2022-10-25 23:24:59 +0700

    command line arguments

---
+++ co_concat_files.py
@@ -0,0 +1,7 @@
+# https://pythonbasics.org/read-excel/
+
+import pandas as pd
+
+df = pd.read_excel("D:\\share\\data\\expense\\012022_Data_TRN_EXPENSE_Part1.xlsx")
+print(df)
+print()
---
+++ command_line_arguments.py
@@ -0,0 +1,20 @@
+# Python program to demonstrate
+# command line arguments
+
+
+import argparse
+
+
+# Initialize parser
+parser = argparse.ArgumentParser()
+
+# Adding optional argument
+# parser.add_argument("-o", "--Output", help = "Show Output")
+parser.add_argument("File", metavar="revenue file", type=str, help="Input file")
+# Read arguments from command line
+args = parser.parse_args()
+input_file = args.File
+
+if args.File:
+    print("Displaying Output as: % s" % args.File)
+    print("Input file %s" % input_file)
